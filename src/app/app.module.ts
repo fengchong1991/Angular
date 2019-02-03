5a08500e0c09006ae6dc3ece52f533aef6ea499d
Chong Feng <chong.feng@koresoftware.com>
2019-02-03 22:50:43 +1100

    Add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav/navbar.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { EventService } from './events/shared/event.service';
 import { ToastrService } from './common/toastr.service';
-
+import { EventDetailsComponent } from './events/event-details/event-details.component';
+import { appRoutes } from './routes'
+import { RouterModule } from '@angular/router';
+import { CreateEventComponent } from './events/create-event.component'
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     EventsListComponent,
     EventThumbnailComponent,
+    EventDetailsComponent,
+    CreateEventComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [EventService, ToastrService],
   bootstrap: [AppComponent]
